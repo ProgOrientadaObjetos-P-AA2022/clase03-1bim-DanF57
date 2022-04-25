ce8619975053add757a98ea344e59adad16e3751
DanF57 <danser2002@hotmail.com>
2022-04-25 08:37:16 -0500

    Material de Clase (Objetos Intro)

--- ManejoClases/src/manejoclases03/Ejecutable04.java
+++ ManejoClases/src/manejoclases03/Ejecutable04.java
@@ -21,14 +21,27 @@ public class Ejecutable04 {
         double valor2 = 20000;
         double valor3 = 30000;
         
+        int camas1 = 300;
+        int camas2 = 400;
+        int camas3 = 781;
+        
         h1.establecerPresupuesto(valor1);
         h2.establecerPresupuesto(valor2);
         h3.establecerPresupuesto(valor3);
         
+        h1.establecerNumeroCamas(camas1);
+        h2.establecerNumeroCamas(camas2);
+        h3.establecerNumeroCamas(camas3);
+        
         double suma = h1.obtenerPresupuesto() + h2.obtenerPresupuesto() +
                 h3.obtenerPresupuesto();
         
+        int suma2 = h1.obtenerNumeroCamas() + h2.obtenerNumeroCamas() +
+                h3.obtenerNumeroCamas();
+        
         System.out.printf("La suma de presupuestos es %.2f\n", suma);
+        
+        System.out.printf("La suma de camas es %d\n", suma2);
         
         
     }
